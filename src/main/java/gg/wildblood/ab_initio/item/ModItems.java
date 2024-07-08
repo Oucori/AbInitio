3e69ed650001b9e1005f662f8152678a4412d3b6
q.weidenboerner <quentin.weidenboerner@mait.de>
2024-07-08 09:33:24 +0200

    Fixed Bracket

--- src/main/java/gg/wildblood/ab_initio/item/ModItems.java
+++ src/main/java/gg/wildblood/ab_initio/item/ModItems.java
@@ -17,13 +17,14 @@ public class ModItems {
 	public static final Item CLAY_BUCKET_RAW = new Item(new QuiltItemSettings().maxCount(16));
 	public static final Item CLAY_BUCKET = new ClayBucket(Fluids.EMPTY, new QuiltItemSettings().maxCount(16));
 	public static final Item CLAY_WATER_BUCKET = new ClayBucket(Fluids.WATER, new QuiltItemSettings().maxCount(1));
-	public static final Item CLAY_LAVA_BUCKET = new ClayBucket(Fluids.LAVA,new QuiltItemSettings().maxCount(1));
-  public static final ShearsItem WOODEN_SHEARS = new ShearsItem(new QuiltItemSettings().maxCount(1).maxDamage(30));
+	public static final Item CLAY_LAVA_BUCKET = new ClayBucket(Fluids.LAVA, new QuiltItemSettings().maxCount(1));
+	public static final ShearsItem WOODEN_SHEARS = new ShearsItem(new QuiltItemSettings().maxCount(1).maxDamage(30));
 
 	public static void register(ModContainer mod) {
 		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "clay_bucket_raw"), CLAY_BUCKET_RAW);
 		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "clay_bucket"), CLAY_BUCKET);
 		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "clay_water_bucket"), CLAY_WATER_BUCKET);
 		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "clay_lava_bucket"), CLAY_LAVA_BUCKET);
-    Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "wooden_shears"), WOODEN_SHEARS);
+		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "wooden_shears"), WOODEN_SHEARS);
+	}
 }
